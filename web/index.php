e58369a06e8a6ed84ea34d8aa79b92b65856e3ac
Agent W <bakury19004@gmail.com>
2018-04-26 16:03:14 -0600

    pointing to hello page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
 <?php
 
-  phpinfo();
+  <html>
+<head>
+    <title>Redirecting...</title>
+    <meta http-equiv="refresh" content="0;URL=hello.html">
+</head>
+<body>
+    You are being automatically redirected to a new location.<br />
+    If your browser does not redirect you in few seconds, or you do
+    not wish to wait, <a href="hello.html">click here</a>. 
+</body>
+</html>
 
 ?>
