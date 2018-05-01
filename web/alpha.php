c563576585df7c46133c978deebe05f2539c883e
Agent W <bakury19004@gmail.com>
2018-05-01 16:35:51 -0600

    testing php page

---
+++ web/alpha.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>
+            Alpha Alpha
+        </title>
+    </head>
+    <body>
+        <h1>This is a PHP page</h1>
+        <?php
+        echo "I love you."
+    ?>
+    </body>
+</html> 
