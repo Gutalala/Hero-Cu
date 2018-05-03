089680f9ffb6cd586b8eb5bd78298eedef1870ae
Agent W <bakury19004@gmail.com>
2018-05-03 16:19:41 -0600

    made bunch of php

---
+++ web/header.php
@@ -0,0 +1,32 @@
+  <style>
+         ul.top-links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333333;
+  }
+            ul.top-links li {
+    float: right;
+  }
+
+  ul.top-links li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    padding-right: 30px;
+    padding-left: 30px;
+    text-decoration: none;
+}
+
+  ul.top-links li a:hover {
+    background-color: #111111;
+}
+        </style>
+
+        <ul class="top-links">
+  <li><a href="home.php">HOME</a></li>
+  <li><a href="login.php">LOGIN</a></li>
+  <li><a href="aboutpage.php">ABOUT US</a></li>
+        </ul>
