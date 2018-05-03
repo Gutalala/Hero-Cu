2f55be4c88791792deb144cb18700bab061c251d
Agent W <bakury19004@gmail.com>
2018-05-03 16:10:17 -0600

    fixed top links

--- web/home.php
+++ web/home.php
@@ -12,11 +12,28 @@
     overflow: hidden;
     background-color: #333333;
   }
+            ul.top-links li {
+    float: right;
+  }
+
+  ul.top-links li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    padding-right: 30px;
+    padding-left: 30px;
+    text-decoration: none;
+}
+
+  ul.top-links li a:hover {
+    background-color: #111111;
+}
         </style>
     </head>
     <body>
         <ul class="top-links">
-  <li><a href="home.html">HOMEE</a></li>
+  <li><a href="home.html">HOME</a></li>
   <li><a href="login.html">LOGIN</a></li>
   <li><a href="aboutpage.html">ABOUT US</a></li>
         </ul>
