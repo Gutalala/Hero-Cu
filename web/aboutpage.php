facbc8c2c1bcdd09c98ac2ec8fe14b09174c31f8
Agent W <bakury19004@gmail.com>
2018-05-03 16:26:16 -0600

    refining pages

---
+++ web/aboutpage.php
@@ -0,0 +1,10 @@
+
+<?php
+include 'header.php';
+?>
+
+<div>
+    <h1>
+        WE ARE A TEAM OF THREE. THAT'S RIGHT. A CROWD YOU MIGHT CALL US. BUT YOU WON'T FIND ANOTHER CROWD AS PRETTY AS US!
+    </h1>
+</div>
---
+++ web/login.php
@@ -0,0 +1,10 @@
+<?php
+include 'header.php';
+?>
+
+
+<div>
+    <h1>
+        PLEASE LOGIN
+    </h1>
+</div>
