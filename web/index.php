733629acd32802878dd84200eaa3203f675c6f9d
Agent W <bakury19004@gmail.com>
2018-05-08 18:26:38 -0600

    changed to inline css

--- web/index.php
+++ web/index.php
@@ -42,6 +42,62 @@
       font-family: Helvetica Neue;
       font-weight: bold;
     }
+    
+    ul.top-links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333333;
+  }
+
+  ul.top-links li {
+    float: right;
+  }
+
+  ul.top-links li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    padding-right: 30px;
+    padding-left: 30px;
+    text-decoration: none;
+}
+
+  ul.top-links li a:hover {
+    background-color: #111111;
+}
+
+ul.mid-links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: white;
+  }
+
+  ul.mid-links li {
+    float: left;
+  }
+
+  ul.mid-links li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 16px;
+    padding-right: 30px;
+    padding-left: 30px;
+    text-decoration: none;
+    font-size: large;
+    font-weight: 500;
+    border-top: solid black;
+}
+
+ul.mid-links li a:hover {
+    color: blue;
+}
+
 
 </style>
 
