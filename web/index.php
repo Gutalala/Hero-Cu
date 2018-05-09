3683bb262672a506ae48c328c68f80176d935bd9
Agent W <bakury19004@gmail.com>
2018-05-08 19:17:35 -0600

    overhaul bug fixes

--- web/index.php
+++ web/index.php
@@ -5,10 +5,13 @@
        <title>ANEA Official Website</title>
        <meta charset="utf-8">   
        <style>
-           <?php include 'top-header.php' ?>
+           <?php include 'style.css' ?>
        </style>    
    </head>
    <body>
+   
+   <?php include 'top-header.php' ?>
+   
 <ul class="top-links">
   <li><a href="shop.html">CATALOG</a></li>
   <li><a href="#register">REGISTER</a></li>
