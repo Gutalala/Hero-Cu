dde0254dd780f898acc523d2bb4872762dc5aa3b
Agent W <bakury19004@gmail.com>
2018-05-15 01:27:46 -0600

    parallel elements

--- web/cart.php
+++ web/cart.php
@@ -12,25 +12,28 @@
       <div class="column col1">
         <ul style="list-style-type: none;">
           <li id="prod1"><img class="products" src="nanoalpha.jpg" alt="alpha" width="300" height="300"></li>
-          <li style="text-align: left;"> The Original Suit - Ground Combat Specialist</li>
           <br>
           <hr>
           <li id="prod2"><img class="products" src="nanobeta.jpg" alt="beta" width="300" height="300"></li>
-          <li style="text-align: left;"> The Derivative Suit - Designed for Underwater</li>
           <br>
           <hr>
           <li id="prod3"><img class="products" src="nanosuit1.jpg" alt="nano1" width="300" height="300"></li>
-          <li style="text-align: left;"> The Edge Suit - Adapt to All Combat </li>
           <br>
           <hr>
         </ul>
+      </div>
+      <div class="column col2">
+          <ul style="list-style-type: none;">
+              <li> The Original Suit - Ground Combat Specialist</li>
+              <li> The Derivative Suit - Designed for Underwater</li>
+              <li> The Edge Suit - Adapt to All Combat </li>     
+          </ul>
       </div>          
-      <div class="column col4">
+      <div class="column col3">
         <ul class="products">
           <form id="form2">
           <li><input id="quantity1" class="quantity" 
                      placeholder="0" size="3"></li>
-                     <a>remove</a>
           <li><input id="quantity2" class="quantity"  
                      placeholder="0" size="3"></li>
           <li><input id="quantity3" class="quantity" 
