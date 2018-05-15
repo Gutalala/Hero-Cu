b830d2b3b9945a555c28c3e56424a7dbafc8203b
Agent W <bakury19004@gmail.com>
2018-05-15 02:27:05 -0600

    changes changes

--- web/shop.php
+++ web/shop.php
@@ -13,14 +13,17 @@
  <div class="row">
   <div class="column">
      <img class="products" src="nanoalpha.jpg" alt="alpha" width="300" height="300">
+     <br>
      <button>ADD</button>
   </div>
   <div class="column">
     <img class="products" src="nanobeta.jpg" alt="beta" width="300" height="300">
+    <br>
     <button>ADD</button>
   </div>
   <div class="column">
     <img class="products" src="nanosuit1.jpg" alt="nano1" width="300" height="300">
+    <br>
     <button>ADD</button>
   </div>
 </div>
