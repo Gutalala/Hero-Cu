400f9705eb18466c698a0336c110c7d9a09eb00e
Agent W <bakury19004@gmail.com>
2018-05-16 13:27:35 -0600

    making add to cart

--- web/shop.php
+++ web/shop.php
@@ -1,36 +1,5 @@
 <?php
    session_start();
-$_SESSION["original"] = 0;
-$_SESSION["derivative"] = 0;
-$_SESSION["edge"] = 0;
-
-if (isset($_POST['action'])) {
-    switch ($_POST['action']) {
-        case 'add1':
-            alert("hiela");
-            break;
-        case 'add2':
-            add2();
-            break;
-        case 'add3':
-            add3();
-            break;
-    }
-}
-
-function alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-}
-
-function add2() {
-    echo "The insert function is called.";
-    exit;
-}
-
-function add3() {
-    echo "The lol function is called.";
-    exit;
-}
 ?>
 
 <!DOCTYPE html>
@@ -46,7 +15,7 @@ function add3() {
     ?>
     <h1 style="text-align: center";><strong>THE STORE</strong></h1>
  <div class="row">
- <form action="shop.php" method="post">
+ <form action="shopadd.php" method="post">
   <div class="column">
      <img class="products" src="nanoalpha.jpg" alt="alpha" style='height: 100%; width: 100%; object-fit: contain; padding-bottom: 20px'>
      <br>
