f0d28ae04dceada4537edf4b00775572529d05fb
Agent W <bakury19004@gmail.com>
2018-05-15 23:26:16 -0600

    making add to cart

--- web/cart.php
+++ web/cart.php
@@ -34,38 +34,3 @@ function add3() {
 ?>
 
 
-
-<!DOCTYPE html>
-<html lang="">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>YOUR SHOPPING CART</title>
-  <link href="cartStyle.css" type="text/css" rel="stylesheet">
-</head>
-
-<body>
-     <?php
-    include 'header.php';
-    ?>
-      <div class="column col2">
-          <ul class="productsName">
-              <li> Original Suit </li>
-              <li> Derivative Suit </li>
-              <li> Edge Suit </li>     
-          </ul>
-      </div>          
-      <div class="column col3">
-        <ul class="products">
-          <form id="form2">
-          <li><input id="quantity1" class="quantity" 
-                     placeholder="0" size="3"></li>
-          <li><input id="quantity2" class="quantity"  
-                     placeholder="0" size="3"></li>
-          <li><input id="quantity3" class="quantity" 
-                     placeholder="0" size="3"></li>
-          </form>
-       </ul>
-     </div>
-</body>
-</html>
