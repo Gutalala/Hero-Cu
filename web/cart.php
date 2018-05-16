818ed2fb886c77175d5df5e39b95829444b4afbc
Agent W <bakury19004@gmail.com>
2018-05-15 23:32:12 -0600

    making add to cart

--- web/cart.php
+++ web/cart.php
@@ -1,36 +1,34 @@
-<?php
-$_SESSION["original"] = 0;
-$_SESSION["derivative"] = 0;
-$_SESSION["edge"] = 0;
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>YOUR SHOPPING CART</title>
+  <link href="cartStyle.css" type="text/css" rel="stylesheet">
+</head>
 
-if (isset($_POST['action'])) {
-    switch ($_POST['action']) {
-        case 'add1':
-            add1();
-            break;
-        case 'add2':
-            add2();
-            break;
-        case 'add3':
-            add3();
-            break;
-    }
-}
-
-function add1() {
-    echo "The select function is called.";
-    exit;
-}
-
-function add2() {
-    echo "The insert function is called.";
-    exit;
-}
-
-function add3() {
-    echo "The lol function is called.";
-    exit;
-}
-?>
-
-
+<body>
+     <?php
+    include 'header.php';
+    ?>
+      <div class="column col2">
+          <ul class="productsName">
+              <li> Original Suit </li>
+              <li> Derivative Suit </li>
+              <li> Edge Suit </li>     
+          </ul>
+      </div>          
+      <div class="column col3">
+        <ul class="products">
+          <form id="form2">
+          <li><input id="quantity1" class="quantity" 
+                     placeholder="0" size="3"></li>
+          <li><input id="quantity2" class="quantity"  
+                     placeholder="0" size="3"></li>
+          <li><input id="quantity3" class="quantity" 
+                     placeholder="0" size="3"></li>
+          </form>
+       </ul>
+     </div>
+</body>
+</html>
--- web/shop.php
+++ web/shop.php
@@ -1,5 +1,37 @@
 <?php
    session_start();
+$_SESSION["original"] = 0;
+$_SESSION["derivative"] = 0;
+$_SESSION["edge"] = 0;
+
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'add1':
+            add1();
+            break;
+        case 'add2':
+            add2();
+            break;
+        case 'add3':
+            add3();
+            break;
+    }
+}
+
+function add1() {
+    echo "The select function is called.";
+    exit;
+}
+
+function add2() {
+    echo "The insert function is called.";
+    exit;
+}
+
+function add3() {
+    echo "The lol function is called.";
+    exit;
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +47,7 @@
     ?>
     <h1 style="text-align: center";><strong>THE STORE</strong></h1>
  <div class="row">
- <form action="cart.php" method="post">
+ <form action="shop.php" method="post">
   <div class="column">
      <img class="products" src="nanoalpha.jpg" alt="alpha" style='height: 100%; width: 100%; object-fit: contain; padding-bottom: 20px'>
      <br>
