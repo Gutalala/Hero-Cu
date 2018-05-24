b9a5b93691baddccb4ea8696495c62e196b74870
Agent W <bakury19004@gmail.com>
2018-05-24 16:33:43 -0600

    sql movies

---
+++ week05_movies/movies.php
@@ -0,0 +1,38 @@
+<?php
+
+try{
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $ex){
+echo 'Error!: ' . $ex->getMessage();
+die();
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MOVIES BABIES</title>
+</head>
+<body>
+ 	<h1 style="text-align: center;">MOVIES LIST</h1>
+ 	<ul>
+ 		<li>KING OF HEARTS</li>
+ 	</ul>
+</body>
+</html> 
