70d688a642433f5e88e2bcf0ce3e00eff6909207
Agent W <bakury19004@gmail.com>
2018-05-31 16:25:40 -0600

    probably not that important, ran from shell

--- web/courseDetails.php
+++ web/courseDetails.php
@@ -6,8 +6,12 @@ $courseId = htmlspecialchars($_GET["course_id"]);
 
 $db = get_db();
 
-$query = "SELECT name, number FROM course WHERE id=$id";
+$query = "SELECT name, number FROM course WHERE id=:id";
 
+$statement = $db->prepare($query);
+$statement->execute();
+$row = $statement->fetch();
 
+var_dump($row);
 
 ?>
