bed35df5aee59c6218730a02a263a6e483e47f75
Agent W <bakury19004@gmail.com>
2018-05-31 16:07:42 -0600

    probably not that important, ran from shell

--- courseExer.php
+++ courseExer.php
@@ -1,21 +1,3 @@
-<?php
-
- require("dbConnect.php");
-
- $db = get_db();
- if (!isset($db)) {
- 	# code...
- 	die("DB Connection was not set");
- }
-
- $query = "SELECT id, name, number FROM course";
-
-$statement = $db->prepare($query);
-$statement->execute();
-
-$courses = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-?>
 
 <!DOCTYPE html>
 <html>
@@ -25,9 +7,7 @@ $courses = $statement->fetchAll(PDO::FETCH_ASSOC);
 <body>
  <h1>COURSES</h1>
  <p>
- 	<?php
- 	var_dump($courses);
- 	?>
+ 	Testing
 </p>
 </body>
 </html>
