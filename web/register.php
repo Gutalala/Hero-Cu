896c85e0be1ab5343caf63737f557288a60a6efe
Agent W <bakury19004@gmail.com>
2018-06-05 16:34:37 -0600

    probably not that important, ran from shell

--- web/register.php
+++ web/register.php
@@ -63,14 +63,14 @@ body {
 </head>
 <body>
   <div class="wrapper">
-    <form class="form-signup">       
+    <form class="form-signup" action="create_user.php" method="post">       
       <h2 class="form-signup-heading">Sign Up</h2>
       <input type="text" class="form-control" name="username" placeholder="Username" required="" autofocus="" />
       <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
       <label class="checkbox">
         <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
       </label>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>   
+      <input class="btn btn-lg btn-primary btn-block" type="submit">Register</button>   
     </form>
   </div>
 </body>
