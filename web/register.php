b08f1da90f9d5eff47cd2f49312bfb49b39d93ed
Agent W <bakury19004@gmail.com>
2018-06-05 16:18:41 -0600

    probably not that important, ran from shell

--- web/register.php
+++ web/register.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 	<title>Register Page</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 	<style type="text/css">
 		@import url(http://fonts.googleapis.com/css?family=Roboto);
 
@@ -102,11 +106,6 @@
 	</style>
 </head>
 <body>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
 <a href="#" data-toggle="modal" data-target="#login-modal">Login</a>
 
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
