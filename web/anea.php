3d3c3229e16508d2ff8881bc2f77ad2da66a8490
Agent W <bakury19004@gmail.com>
2018-06-07 21:53:35 -0600

    probably not that important, ran from shell

---
+++ web/anea.php
@@ -0,0 +1,25 @@
+<ul class="top-links">
+  <li><a href="shop.html">CATALOG</a></li>
+  <li><a href="#register">REGISTER</a></li>
+  <li><a href="login.html">LOGIN</a></li>
+  <li style="float: left"><a href="welcome.html";">HOME</a></li>
+</ul> 
+
+    <div id="Welcome" class="headline">
+    <h1>LA PORTFOLIO I</h1>
+    </div>
+    <div id="symbol">
+    <p style="text-align: center;"> 
+      <!-- <a href="secondpage.html"> -->
+      <img src="triqueta.jpg" alt="Company Symbol" width="350" height="350"> 
+    <!-- </a> -->
+  </div>
+
+  <ul class="mid-links" style="margin-left: 450px">
+  <li><a href="#news">NEWS BLOG</a></li>
+  <li><a href="#products">PRODUCTS</a></li>
+  <li><a href="#careers">INVESTORS</a></li>
+  <li><a href="#about";">ABOUT CREATOR</a></li>
+  <li><a href="#contact";">CONTACTS</a></li>
+
+</ul> 
--- web/index.php
+++ web/index.php
@@ -12,31 +12,6 @@
    
    <?php include 'top-header.php' ?>
    
-<ul class="top-links">
-  <li><a href="shop.html">CATALOG</a></li>
-  <li><a href="#register">REGISTER</a></li>
-  <li><a href="login.html">LOGIN</a></li>
-  <li style="float: left"><a href="welcome.html";">HOME</a></li>
-</ul> 
-
-    <div id="Welcome" class="headline">
-    <h1>LA PORTFOLIO I</h1>
-    </div>
-    <div id="symbol">
-    <p style="text-align: center;"> 
-      <!-- <a href="secondpage.html"> -->
-      <img src="triqueta.jpg" alt="Company Symbol" width="350" height="350"> 
-    <!-- </a> -->
-  </div>
-
-  <ul class="mid-links" style="margin-left: 450px">
-  <li><a href="#news">NEWS BLOG</a></li>
-  <li><a href="#products">PRODUCTS</a></li>
-  <li><a href="#careers">INVESTORS</a></li>
-  <li><a href="#about";">ABOUT CREATOR</a></li>
-  <li><a href="#contact";">CONTACTS</a></li>
-
-</ul> 
     <div id = "mission">
       <h2>WELCOME TO MY HOMEPAGE. I'M INTERESTED IN MAKING TOP-QUALITY INTERACTIVE WEBSITE AND WIN A CSS AWARD OR TWO.</h2>
     </div>
