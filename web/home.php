bf6ba601e8c635b53e9a3aa861cbe94cf9d4ef7f
Agent W <bakury19004@gmail.com>
2018-06-12 23:40:52 -0600

    probably not that important, ran from shell

--- web/home.php
+++ web/home.php
@@ -1,4 +1,23 @@
-
+<?php
+/**********************************************************
+* File: home.php
+* Author: Br. Burton
+* 
+* Description: This is the home page. It checks that a user
+*  exists on the session and redirects to the login page
+*  if it does not.
+***********************************************************/
+session_start();
+if (isset($_SESSION['username']))
+{
+	$username = $_SESSION['username'];
+}
+else
+{
+	header("Location: login.php");
+	die(); // we always include a die after redirects.
+}
+?>
 
 <!DOCTYPE html><html class=''>
 <head>
