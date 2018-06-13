aa62c07d6b476241071a59eb6244e42c81fab935
Agent W <bakury19004@gmail.com>
2018-06-12 23:55:55 -0600

    probably not that important, ran from shell

--- web/home.php
+++ web/home.php
@@ -38,10 +38,37 @@ body{
 	overflow:hidden;
 	width:100%;
 }
+ul.top-links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333333;
+  }
+ul.top-links li {
+    float: right;
+  }
+
+ul.top-links li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    padding-right: 30px;
+    padding-left: 30px;
+    text-decoration: none;
+}
+
+ul.top-links li a:hover {
+    background-color: #111111;
+}
 
 </style>
 </head>
 
-<body>
-
+<body>       
+    <ul class="top-links">
+        <li><a class="store" href="logout.php">LOG OUT</a></li>
+        </ul>
+        
 </body></html>
