772f34ec9007e834b6d7b2cfc49b7401d38cf131
Agent W <bakury19004@gmail.com>
2018-06-15 04:13:50 -0600

    probably not that important, ran from shell

--- web/editPosts.php
+++ web/editPosts.php
@@ -29,10 +29,6 @@ foreach ($posts as $Posts) {
     	$post_date = $Posts["post_date"];
     }
 
-$query3 = "DELETE FROM Posts WHERE id=:post_id";
-$post_delete = $db->prepare($query3);
-$post_delete->bindValue(":post_id", $post_id, PDO::PARAM_INT);
-$post_delete->execute();
 ?>
 
 
